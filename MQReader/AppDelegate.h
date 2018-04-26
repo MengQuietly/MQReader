5da82cc187abb532a1d0a4eb4baa5e3ac04879e3
Meng <mengmeng@mengdemacbook-pro.local>
2018-04-26 17:14:10 +0800

    Initial Commit

---
+++ MQReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MQReader
+//
+//  Created by Meng on 2018/4/26.
+//  Copyright © 2018年 Meng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MQReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MQReader
+//
+//  Created by Meng on 2018/4/26.
+//  Copyright © 2018年 Meng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
